fb4414c8a0316b2c3367115d759662e6379585c1
Ryan "R.K." Kwon <rk.ryankwon@gmail.com>
2018-12-01 13:53:02 -0800

    onclick() => search

--- filter.js
+++ filter.js
@@ -1,49 +1,43 @@
- var filter = document.getElementById('maskButton');
+var filter = document.getElementById('maskButton');
 
- filter.onclick = function(element) {
-    // console.log("s");
-    search(document.body);
-  };
+filter.onclick = function (element) {
+    alert('mask button clicked');
 
+    chrome.tabs.executeScript({
+        code: '(' + fn + ')();'
+    });
 
+    function fn() {
+        alert('fuck');
+        console.log('bitch');
+        search(document.body);
 
-  function search(htmlElem) {
+        function search(htmlElem) {
+            var subElem;
+            var next;
 
-    var subElem;
-    var next;
+            switch (htmlElem.nodeType) {
+                case 1:  // Element
+                case 9:  // Document
+                case 11: // Document fragment
+                    subElem = htmlElem.firstChild;
+                    while (subElem) {
+                        next = subElem.nextSibling;
+                        search(subElem);
+                        subElem = next;
+                    }
+                    break;
+                case 3: // Text node
+                    handleText(htmlElem);
+                    break;
+            }
+        }
 
-    
-
-    switch(htmlElem.nodeType) {
-        case 1:  // Element
-		case 9:  // Document
-        case 11: // Document fragment
-			subElem = htmlElem.firstChild;
-			while (subElem) 
-			{
-				next = subElem.nextSibling;
-				search(subElem);
-				subElem = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(htmlElem);
-			break;
+        function handleText(textNode) {
+            var v = textNode.nodeValue;
+            alert(v);
+            v = v.replace(/\bfuck\b/g, "duck");
+            textNode.nodeValue = v;
+        }
     }
-}
-
-
-function handleText(textNode) 
-{
-
-  var v = textNode.nodeValue;
-  
-  alert(v);
-  
-
-
-	v = v.replace(/\bfuck\b/g, "duck");
-	
-	textNode.nodeValue = v;
-}
+};
