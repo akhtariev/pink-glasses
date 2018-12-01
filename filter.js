4a9bf1ffeba4ecf13ca4e6dc35c7d2d2a3043c18
Roman Akhtariev <ackhtariev@gmail.com>
2018-12-01 13:28:51 -0800

    Change *uck to duck on click

---
+++ filter.js
@@ -0,0 +1,49 @@
+ var filter = document.getElementById('filter');
+
+ filter.onclick = function(element) {
+    // console.log("s");
+    search(document.body);
+  };
+
+
+
+  function search(htmlElem) {
+
+    var subElem;
+    var next;
+
+    
+
+    switch(htmlElem.nodeType) {
+        case 1:  // Element
+		case 9:  // Document
+        case 11: // Document fragment
+			subElem = htmlElem.firstChild;
+			while (subElem) 
+			{
+				next = subElem.nextSibling;
+				search(subElem);
+				subElem = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(htmlElem);
+			break;
+    }
+}
+
+
+function handleText(textNode) 
+{
+
+  var v = textNode.nodeValue;
+  
+  alert(v);
+  
+
+
+	v = v.replace(/\bfuck\b/g, "duck");
+	
+	textNode.nodeValue = v;
+}
